7b3880a105495be29af036f4ccf456758b79c107
Chris Zhang <chris.qh.zhang@outlook.com>
2025-01-26 00:13:53 +0200

    Integrate resources

---
+++ Assets/Chris/Scripts/ObjectShake.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectShake : MonoBehaviour
+{
+    public Sprite sprite;
+    public void OnMouseDown()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
